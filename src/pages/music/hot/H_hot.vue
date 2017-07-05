e08fefaf84d61cdd52f0978373e430597faf2536
Jemvdg <923565617@qq.com>
2017-07-05 16:11:22 +0800

    upload the musicHot and musicRadio pages files

---
+++ src/pages/music/hot/H_hot.vue
@@ -0,0 +1,133 @@
+<template>
+    <div>
+        <header>
+            <ul class="nav">
+                <li class="sub-nav">
+                    <i class="fa fa-bars"></i>
+                </li>
+                <li>我的</li>
+                <li class="active">音乐</li>
+                <li>动态</li>
+                <li class="search-nav">
+                    <i class="fa fa-search"></i>
+                </li>
+            </ul>
+        </header>
+    
+        <section class="jem-hot-main">
+            <ul class="main-nav">
+                <li class="active">热门推荐</li>
+                <li>榜单</li>
+                <li>歌单</li>
+                <li>电台</li>
+            </ul>
+            <div class="main-content">
+                <img class="banner" src="./hot-banner.png" alt="fff">
+                <div class="classify">
+                    <div class="song-recommend">
+                        <img src="./hot-radio.png" alt="fff">
+                        <p>每日歌曲推荐</p>
+                    </div>
+                    <div class="singer-recommend">
+                        <img src=".//hot-microphone.png" alt="fff">
+                        <p>歌手</p>
+                    </div>
+                    <div class="songtype-recommend">
+                        <img src="./hot-clipboard.png" alt="fff">
+                        <p>歌曲</p>
+                    </div>
+                </div>
+                <div class="recommend-title">
+                    <p class="main-title">
+                        <img src="./hot-music-icon.png" alt="fff">推荐歌单</p>
+                    <p class="sub-title">更多></p>
+                </div>
+                <div class="recommend-songlist">
+                    <div class="recommend-songitem">
+                        <img src="./hot-cover1.png" alt="fff">
+                        <p>苏打绿陪你一起走过 在每一个难忘的瞬间</p>
+                    </div>
+                    <div class="recommend-songitem">
+                        <img src="./hot-cover1.png" alt="fff">
+                        <p>苏打绿陪你一起走过 在每一个难忘的瞬间</p>
+                    </div>
+                    <div class="recommend-songitem">
+                        <img src="./hot-cover1.png" alt="fff">
+                        <p>苏打绿陪你一起走过 在每一个难忘的瞬间</p>
+                    </div>
+                </div>
+                <div class="recommend-songlist">
+                    <div class="recommend-songitem">
+                        <img src="./hot-cover1.png" alt="fff">
+                        <p>苏打绿陪你一起走过 在每一个难忘的瞬间</p>
+                    </div>
+                    <div class="recommend-songitem">
+                        <img src="./hot-cover1.png" alt="fff">
+                        <p>苏打绿陪你一起走过 在每一个难忘的瞬间</p>
+                    </div>
+                    <div class="recommend-songitem">
+                        <img src="./hot-cover1.png" alt="fff">
+                        <p>苏打绿陪你一起走过 在每一个难忘的瞬间</p>
+                    </div>
+                </div>
+                <div class="recommend-title">
+                    <p class="main-title">
+                        <img src="./hot-music-icon.png" alt="fff">推荐歌单</p>
+                    <p class="sub-title">更多></p>
+                </div>
+                <div class="recommend-songlist">
+                    <div class="recommend-songitem">
+                        <img src="./hot-cover1.png" alt="fff">
+                        <p>苏打绿陪你一起走过 在每一个难忘的瞬间</p>
+                    </div>
+                    <div class="recommend-songitem">
+                        <img src="./hot-cover1.png" alt="fff">
+                        <p>苏打绿陪你一起走过 在每一个难忘的瞬间</p>
+                    </div>
+                    <div class="recommend-songitem">
+                        <img src="./hot-cover1.png" alt="fff">
+                        <p>苏打绿陪你一起走过 在每一个难忘的瞬间</p>
+                    </div>
+                </div>
+                <div class="recommend-songlist">
+                    <div class="recommend-songitem">
+                        <img src="./hot-cover1.png" alt="fff">
+                        <p>苏打绿陪你一起走过 在每一个难忘的瞬间</p>
+                    </div>
+                    <div class="recommend-songitem">
+                        <img src="./hot-cover1.png" alt="fff">
+                        <p>苏打绿陪你一起走过 在每一个难忘的瞬间</p>
+                    </div>
+                    <div class="recommend-songitem">
+                        <img src="./hot-cover1.png" alt="fff">
+                        <p>苏打绿陪你一起走过 在每一个难忘的瞬间</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    
+        <footer>
+            <img class="jem-cover" src="./my-cover.png" alt="">
+            <div class="jem-song-info">
+                <p class="jem-song-name">不该（with AMEI）</p>
+                <p class="jem-song-singer">周杰伦</p>
+            </div>
+            <div class="jem-play-ctrl">
+                <i class="fa fa-play"></i>
+                <i class="fa fa-step-forward"></i>
+                <i class="fa fa-list"></i>
+            </div>
+        </footer>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+
+</script>
+
+<style lang="scss">
+@import '../../../scss/_H_hot.scss';
+</style>
---
+++ src/pages/music/radio/J_radio.vue
@@ -0,0 +1,161 @@
+<template>
+    <div>
+        <header>
+            <ul class="nav">
+                <li class="sub-nav">
+                    <i class="fa fa-bars"></i>
+                </li>
+                <li>我的</li>
+                <li class="active">音乐</li>
+                <li>动态</li>
+                <li class="search-nav">
+                    <i class="fa fa-search"></i>
+                </li>
+            </ul>
+        </header>
+    
+        <section class="yao-radio-main">
+            <ul class="main-nav">
+                <li>热门推荐</li>
+                <li>榜单</li>
+                <li>歌单</li>
+                <li class="active">电台</li>
+            </ul>
+            <div class="main-content">
+                <img class="banner" src="./radio-banner.png" alt="fff">
+                <div class="classify">
+                    <div class="raido-recommend radio-classify">
+                        <img src="./radio-radio.png" alt="fff">
+                        <p>每日歌曲推荐</p>
+                    </div>
+                    <div class="raido-recommend radio-ranking">
+                        <img src=".//radio-microphone.png" alt="fff">
+                        <p>歌手</p>
+                    </div>
+                </div>
+                <div class="recommend-title">
+                    电台个性推荐
+                </div>
+                <div class="recommend-songlist">
+                    <div class="recommend-songitem">
+                        <div class="item-cover">
+                            <img src="./radio-cover1.png" alt="fff">
+                            <p class="cover-info">李卤味</p>
+                        </div>
+                        <p class="cover-describe">苏打绿陪你一起走过 在每一个难忘的瞬间</p>
+                    </div>
+                    <div class="recommend-songitem">
+                        <div class="item-cover">
+                            <img src="./radio-cover1.png" alt="fff">
+                            <p class="cover-info">李卤味</p>
+                        </div>
+                        <p class="cover-describe">苏打绿陪你一起走过 在每一个难忘的瞬间</p>
+                    </div>
+                    <div class="recommend-songitem">
+                        <div class="item-cover">
+                            <img src="./radio-cover1.png" alt="fff">
+                            <p class="cover-info">李卤味</p>
+                        </div>
+                        <p class="cover-describe">苏打绿陪你一起走过 在每一个难忘的瞬间</p>
+                    </div>
+                </div>
+                <div class="recommend-songlist">
+                    <div class="recommend-songitem">
+                        <div class="item-cover">
+                            <img src="./radio-cover1.png" alt="fff">
+                            <p class="cover-info">李卤味</p>
+                        </div>
+                        <p class="cover-describe">苏打绿陪你一起走过 在每一个难忘的瞬间</p>
+                    </div>
+                    <div class="recommend-songitem">
+                        <div class="item-cover">
+                            <img src="./radio-cover1.png" alt="fff">
+                            <p class="cover-info">李卤味</p>
+                        </div>
+                        <p class="cover-describe">苏打绿陪你一起走过 在每一个难忘的瞬间</p>
+                    </div>
+                    <div class="recommend-songitem">
+                        <div class="item-cover">
+                            <img src="./radio-cover1.png" alt="fff">
+                            <p class="cover-info">李卤味</p>
+                        </div>
+                        <p class="cover-describe">苏打绿陪你一起走过 在每一个难忘的瞬间</p>
+                    </div>
+    
+                </div>
+                <div class="recommend-songlist">
+                    <div class="recommend-songitem">
+                        <div class="item-cover">
+                            <img src="./radio-cover1.png" alt="fff">
+                            <p class="cover-info">李卤味</p>
+                        </div>
+                        <p class="cover-describe">苏打绿陪你一起走过 在每一个难忘的瞬间</p>
+                    </div>
+                    <div class="recommend-songitem">
+                        <div class="item-cover">
+                            <img src="./radio-cover1.png" alt="fff">
+                            <p class="cover-info">李卤味</p>
+                        </div>
+                        <p class="cover-describe">苏打绿陪你一起走过 在每一个难忘的瞬间</p>
+                    </div>
+                    <div class="recommend-songitem">
+                        <div class="item-cover">
+                            <img src="./radio-cover1.png" alt="fff">
+                            <p class="cover-info">李卤味</p>
+                        </div>
+                        <p class="cover-describe">苏打绿陪你一起走过 在每一个难忘的瞬间</p>
+                    </div>
+    
+                </div>
+                <div class="recommend-songlist">
+                    <div class="recommend-songitem">
+                        <div class="item-cover">
+                            <img src="./radio-cover1.png" alt="fff">
+                            <p class="cover-info">李卤味</p>
+                        </div>
+                        <p class="cover-describe">苏打绿陪你一起走过 在每一个难忘的瞬间</p>
+                    </div>
+                    <div class="recommend-songitem">
+                        <div class="item-cover">
+                            <img src="./radio-cover1.png" alt="fff">
+                            <p class="cover-info">李卤味</p>
+                        </div>
+                        <p class="cover-describe">苏打绿陪你一起走过 在每一个难忘的瞬间</p>
+                    </div>
+                    <div class="recommend-songitem">
+                        <div class="item-cover">
+                            <img src="./radio-cover1.png" alt="fff">
+                            <p class="cover-info">李卤味</p>
+                        </div>
+                        <p class="cover-describe">苏打绿陪你一起走过 在每一个难忘的瞬间</p>
+                    </div>
+    
+                </div>
+            </div>
+        </section>
+    
+        <footer>
+            <img class="yao-cover" src="./my-cover.png" alt="">
+            <div class="yao-song-info">
+                <p class="yao-song-name">不该（with AMEI）</p>
+                <p class="yao-song-singer">周杰伦</p>
+            </div>
+            <div class="yao-play-ctrl">
+                <i class="fa fa-play"></i>
+                <i class="fa fa-step-forward"></i>
+                <i class="fa fa-list"></i>
+            </div>
+        </footer>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+
+</script>
+
+<style lang="scss">
+@import '../../../scss/_J_radio.scss';
+</style>
