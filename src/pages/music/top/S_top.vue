e56882542065b7093d176ff604397696bcf629d7
cjy <976509300@qq.com>
2017-07-06 18:22:09 +0800

    update S-top page

--- src/pages/music/top/S_top.vue
+++ src/pages/music/top/S_top.vue
@@ -1,75 +1,75 @@
 <template>
-<!--音乐>歌单 的页面组件
-作者：陈嘉验-->
+    <!--音乐>歌单 的页面组件
+    作者：陈嘉验-->
     <main>
         <div class="container">
-            <ul class="c-top" >
-
-                <li class="c-item" v-for="item,index in newList " >
-                    <div class="c-billboard">
+            <ul class="c-top">
+    
+                <li class="c-item" v-for="item,index in newList">
+                    <div class="c-billboard" >
                         <div class="c-img">
-                            <img :src="item.coverImgUrl" alt="">
+                            <img :src="item.body.result.coverImgUrl" alt="">
                             <i class="fa fa-play-circle-o"></i>
                         </div>
                         <dl>
-                            <dt>{{item.name}}</dt>
+                            <dt>{{item.body.result.name}}</dt>
                             <dd>
-                                <span class="NO-1">1</span>告白气球-　周杰伦</dd>
+                                <span class="NO-1">1</span>{{item.body.result.tracks[0].name}} - {{item.body.result.tracks[0].artists[0].name}}</dd>
                             <dd>
-                                <span class="NO-2">2</span>天若有情 - Lin</dd>
+                                <span class="NO-2">2</span>{{item.body.result.tracks[1].name}} - {{item.body.result.tracks[1].artists[0].name}}</dd>
                             <dd>
-                                <span class="NO-3">3</span>July - 吴亦凡</dd>
+                                <span class="NO-3">3</span>{{item.body.result.tracks[2].name}} - {{item.body.result.tracks[2].artists[0].name}}</dd>
                         </dl>
                     </div>
                     <i class="fa fa-chevron-right c-more"></i>
                 </li>
-              
-                
+    
             </ul>
         </div>
     </main>
 </template>
 <script>
 export default {
-    data(){
-        return{
-            newList:[],
-            hotList:[],
-            originList:[],
+    data() {
+        return {
+            newList: [],
+
         }
     },
-     created() {
-        this.$http.get('http://localhost:3000/top/list?idx=0')
-            .then(function (data) {
-                console.log(data)
-                var result=data.body;
-                this.newList=result;
-                console.log(this.newList,result,1
-                )
-            });
-             this.$http.get('http://localhost:3000/top/list?idx=1')
-            .then(function (data) {
-                console.log(data)
-                var result=data.body;
-                this.hotList=result;
-            });
-             this.$http.get('http://localhost:3000/top/list?idx=2')
-            .then(function (data) {
-                console.log(data)
-                var result=data.body;
-                this.originList=result;
-            });
-            
+    created() {
+        Promise.all([this.$http.get('http://localhost:3000/top/list?idx=0'),
+        this.$http.get('http://localhost:3000/top/list?idx=1'),
+        this.$http.get('http://localhost:3000/top/list?idx=2'),
+        this.$http.get('http://localhost:3000/top/list?idx=3'),
+        this.$http.get('http://localhost:3000/top/list?idx=4'),
+        this.$http.get('http://localhost:3000/top/list?idx=5'),
+        this.$http.get('http://localhost:3000/top/list?idx=6'),
+        this.$http.get('http://localhost:3000/top/list?idx=7'),
+        this.$http.get('http://localhost:3000/top/list?idx=8'),
+        this.$http.get('http://localhost:3000/top/list?idx=9'),
+        this.$http.get('http://localhost:3000/top/list?idx=10'),
+        this.$http.get('http://localhost:3000/top/list?idx=11'),
+        this.$http.get('http://localhost:3000/top/list?idx=12'),
+        this.$http.get('http://localhost:3000/top/list?idx=13'),
+        this.$http.get('http://localhost:3000/top/list?idx=14'),
+        this.$http.get('http://localhost:3000/top/list?idx=15'),
+        this.$http.get('http://localhost:3000/top/list?idx=16'),
+        this.$http.get('http://localhost:3000/top/list?idx=17'),
+        this.$http.get('http://localhost:3000/top/list?idx=18'),
+        this.$http.get('http://localhost:3000/top/list?idx=19'),  
+        this.$http.get('http://localhost:3000/top/list?idx=20'), 
+        this.$http.get('http://localhost:3000/top/list?idx=21'),                       
+        ])
+            .then((datas) => {                
+                this.newList=datas;
+            })
     },
-    
+
 }
 </script>
 <style lang="scss">
-// main {
-//     background: url(./C_list-bg.png) no-repeat;
-// }
-
 .c-top {
+    margin-top: 100px;
     .c-item {
         display: flex;
         justify-content: space-between;
