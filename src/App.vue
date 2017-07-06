8fce48a4c71c48898e5eb383b018006b7e69b785
Jemvdg <923565617@qq.com>
2017-07-06 12:19:13 +0800

    upload the hot and radio

--- src/App.vue
+++ src/App.vue
@@ -2,29 +2,20 @@
   <div id="app">
     <router-view></router-view>
     <!--<myfooter></myfooter>-->
- 
+  
   </div>
 </template>
 
 <script>
 import navTop from './components/J_navTop/navTop'
 import footer from './components/J_footer/footer'
+import hot from './pages/music/hot/H_hot'
 export default {
   name: 'app',
-<<<<<<< HEAD
   components: {
     navTop: navTop,
-=======
-  components:{
-    navTop:navTop,
-<<<<<<< HEAD
-    myfooter:footer,
-=======
-    hot:hot,
->>>>>>> 5b5724ea619920e171c82925c6e976a3df28f904
-    // myfooter:footer,
-
->>>>>>> 91c8edd5d5faf8f3561341003c7f4cae2ec05d07
+    myfooter: footer,
+    hot: hot,
   }
 }
 </script>
--- src/pages/music/music.vue
+++ src/pages/music/music.vue
@@ -8,20 +8,12 @@
 
 <script>
 import navTop from '../../components/J_navTop/navTop'
-<<<<<<< HEAD
 import musicnav from '../../components/music/musicNav/musicNav'
 export default {
   components: {
     navTop, musicnav,
-=======
-
-export default {
-  components:{
-    navTop,
-       
-
->>>>>>> 5b5724ea619920e171c82925c6e976a3df28f904
   }
+
 }
 </script>
 
