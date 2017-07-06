b3e6ca31803e6e3576cd7d4f68c8cb8b095e231c
Killry <574131176@qq.com>
2017-07-06 22:33:21 +0800

    radio wzx-2233

--- src/pages/music/subpages/radioStation.vue
+++ src/pages/music/subpages/radioStation.vue
@@ -1,57 +1,202 @@
 <template>
   <div>
-    <div class="wzx-radio">
+    <banner :banners="banners"></banner>
+    <div class="wzx-radio clearfix">
       <div class="radio-choose clearfix">
         <div class="choose-left">
           <i class="choose-headphone">
                     <img src="../../../assets/img/wzx-headphone.png" alt="">
                 </i>
-          <h3>欢乐颂电台</h3>
-          <br>
-          <h6>当前热度12.3万</h6>
+          <h3>电台分类</h3>
         </div>
         <div class="choose-right choose-left">
           <i class="choose-headphone">
                     <img src="../../../assets/img/wzx-people.png" alt="">
                 </i>
-          <h3>私人电台</h3>
-          <h6>当前热度5.4万</h6>
+          <h3>电台排行</h3>
         </div>
       </div>
       <div class="radio-groom">
         <div class="groom-nav">
-          <img src="../../../assets/img/music.png" alt="">
-          <span>推荐</span>
-          <i class="fa fa-chevron-circle-down"></i>
+          <div class="nav-red"></div>
+          <span>电台个性推荐</span>
         </div>
-        <ul class="groom-ul clearfix">
-          <li>
-            <img src="../../../assets/img/wzx-lowmusic.png" alt="">
-            <h2>轻音乐</h2>
-            <h4>当前热度4.3万</h4>
-          </li>
-          <li><img src="../../../assets/img/wzx-groom-green.png" alt="">
-            <h2>新歌</h2>
-            <h4>当前热度6.9万</h4>
-          </li>
-          <li><img src="../../../assets/img/wzx-book.png" alt="">
-            <h2>新歌</h2>
-            <h4>当前热度3.6万</h4>
-          </li>
-          <li><img src="../../../assets/img/wzx-coffe.png" alt="">
-            <h2>咖啡伴侣</h2>
-            <br>
-            <h4>当前热度5.5万</h4>
-          </li>
-        </ul>
+        <div class="groom-list">
+          <figure class="groom-item" v-for="item,index in recommend">
+            <lazy-image :src="item.picUrl"></lazy-image>
+            <figcaption>{{item.name}}</figcaption>
+          </figure>
+        </div>
       </div>
+      <div class="radio-groom">
+        <div class="groom-nav">
+          <div class="nav-red"></div>
+          <span>商业财经</span>
+        </div>
+        <div class="groom-list">
+          <figure class="groom-item" v-for="item,index in type1">
+            <lazy-image :src="item.picUrl"></lazy-image>
+            <figcaption>{{item.name}}</figcaption>
+          </figure>
+        </div>
+      </div>
+      <div class="radio-groom">
+        <div class="groom-nav">
+          <div class="nav-red"></div>
+          <span>明星做主播</span>
+        </div>
+        <div class="groom-list">
+          <figure class="groom-item" v-for="item,index in type2">
+            <lazy-image :src="item.picUrl"></lazy-image>
+            <figcaption>{{item.name}}</figcaption>
+          </figure>
+        </div>
+      </div>
+      <div class="radio-groom">
+        <div class="groom-nav">
+          <div class="nav-red"></div>
+          <span>音乐故事</span>
+        </div>
+        <div class="groom-list">
+          <figure class="groom-item" v-for="item,index in type3">
+            <lazy-image :src="item.picUrl"></lazy-image>
+            <figcaption>{{item.name}}</figcaption>
+          </figure>
+        </div>
+      </div>
+      <div class="radio-groom">
+        <div class="groom-nav">
+          <div class="nav-red"></div>
+          <span>情感调频</span>
+        </div>
+        <div class="groom-list">
+          <figure class="groom-item" v-for="item,index in type4">
+            <lazy-image :src="item.picUrl"></lazy-image>
+            <figcaption>{{item.name}}</figcaption>
+          </figure>
+        </div>
+      </div>
+      <div class="radio-groom">
+        <div class="groom-nav">
+          <div class="nav-red"></div>
+          <span>有声书</span>
+        </div>
+        <div class="groom-list">
+          <figure class="groom-item" v-for="item,index in type5">
+            <lazy-image :src="item.picUrl"></lazy-image>
+            <figcaption>{{item.name}}</figcaption>
+          </figure>
+        </div>
+      </div>
+      <div class="radio-groom">
+        <div class="groom-nav">
+          <div class="nav-red"></div>
+          <span>电台分类</span>
+        </div>
+      </div>
+      <div class="radio-top " v-for="item,index in catelist">
+        <div class="radio-top-name">{{item.name}}</div>
+      </div>
+
     </div>
   </div>
 </template>
 
 <script>
-    export default {
-        
+import banner from '../../../components/banner'
+import lazyImage from '../../../components/lazyImage'
+export default {
+  components:{
+    banner,
+    'lazy-image':lazyImage
+  },
+  data(){
+    return {
+      isDone: false,
+      banners:[],
+      recommend: [],
+      type1:[],
+      type2:[],
+      type3:[],
+      type4:[],
+      type5:[],
+      catelist:[],
     }
+  },
+   created() {
+      if (!this.isDone) {
+      Promise.all([this.$http.get('http://localhost:3000/banner'),
+      this.$http.get('http://localhost:3000/dj/recommend'),
+      this.$http.get('http://localhost:3000/dj/recommend/type?type=1'),
+      this.$http.get('http://localhost:3000/dj/recommend/type?type=2'),
+      this.$http.get('http://localhost:3000/dj/recommend/type?type=3'),
+      this.$http.get('http://localhost:3000/dj/recommend/type?type=4'),
+      this.$http.get('http://localhost:3000/dj/recommend/type?type=5'),
+      this.$http.get('http://localhost:3000/dj/catelist')
+      ])
+        .then((datas) => {
+          // console.log(datas[7])
+          // console.log(datas[2].body.djRadios[0].category)
+          this.banners = datas[0].data.banners;
+          this.recommend = datas[1].body.djRadios.slice(0,6);
+          this.type1 = datas[2].body.djRadios.slice(0,3);
+          this.type2 = datas[3].body.djRadios.slice(0,3);
+          this.type3 = datas[4].body.djRadios.slice(0,3);
+          this.type4 = datas[5].body.djRadios.slice(0,3);
+          this.type5 = datas[6].body.djRadios.slice(0,3);
+          this.catelist = datas[7].body.categories
+          this.isDone=true;
+        })
+    }
+    // this.$http.get('http://localhost:3000/banner')
+    // .then(data=>{
+    //   if(data.data.code===200){
+    //     this.banners=data.data.banners
+    //   }
+    //   return this.$http.get('http://localhost:3000/dj/recommend/type?type=1')
+    // })
+    // .then(data=>{
+    //   console.log(data.body.djRadios[0])
+    //    if(data.body.code===200){
+    //      this.personalized=data.body.djRadios.slice(0,3)
+    //      console.log(this.personalized)
+    //   }
+    // })
+  }
+}
 </script>
 
+<style lang="scss">
+  .groom-list {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    .groom-item {
+      width: 30%;
+      margin-bottom: 10px;
+      img {
+        max-width: 100%;
+      }
+      figcaption {
+        margin-top: 10px;
+        height: 2.3em;
+        line-height: 1.2;
+        overflow: hidden;
+      }
+    }
+  }
+
+  .radio-top {
+    .radio-top-name {
+      float: left;
+      width: 40%;
+      margin-left: 35px;
+      margin-bottom: 10px;
+      padding-top: 10px;
+      padding-bottom: 5px;
+      color: #b30000;
+      font-size: 16px;
+    }
+  }
+
+</style>
